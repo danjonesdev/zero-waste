a4d6bf44d41681059ca360983eac4eb79dd5b245
Danny Jones <dannyjones360@gmail.com>
2019-03-07 14:17:13 +0000

    First commit

---
+++ prismic-configuration.js
@@ -0,0 +1,26 @@
+module.exports = {
+
+  apiEndpoint: 'https://zero-waste.prismic.io/api/v2',
+
+  // -- Access token if the Master is not open
+  // accessToken: 'xxxxxx',
+
+  // OAuth
+  // clientId: 'xxxxxx',
+  // clientSecret: 'xxxxxx',
+
+  snipcartKey: 'your-snipcart-api-key',
+
+  // -- Links resolution rules
+  // This function will be used to generate links to Prismic.io documents
+  // As your project grows, you should update this function according to your routes
+  linkResolver: function (doc) {
+    if (doc.type == 'category') {
+      return '/category/' + doc.uid;
+    }
+    if (doc.type == 'product') {
+      return '/product/' + doc.uid;
+    }
+    return '/';
+  }
+};
